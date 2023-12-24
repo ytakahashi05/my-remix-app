fa9073d0fcd7b561a23ab65d9c6d6308b4339a16
ytakahashi05 <blackthunder05@gmail.com>
2023-12-24 13:36:17 +0900

    :spartkles: add index and back button

--- app/routes/contacts.$contactId_.edit.tsx
+++ app/routes/contacts.$contactId_.edit.tsx
@@ -1,6 +1,6 @@
 import type { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
-import { Form, useLoaderData } from "@remix-run/react";
+import { Form, useLoaderData, useNavigate } from "@remix-run/react";
 import invariant from "tiny-invariant";
 
 import { getContact, updateContact } from "../data";
@@ -24,6 +24,7 @@ export const loader = async ({ params }: LoaderFunctionArgs) => {
 
 export default function EditContact() {
   const { contact } = useLoaderData<typeof loader>();
+  const navigate = useNavigate();
 
   return (
     <Form id="contact-form" method="post">
@@ -69,7 +70,9 @@ export default function EditContact() {
       </label>
       <p>
         <button type="submit">Save</button>
-        <button type="button">Cancel</button>
+        <button onClick={() => navigate(-1)} type="button">
+          Cancel
+        </button>
       </p>
     </Form>
   );
